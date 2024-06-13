e4f4bda16689291d3bdb8875018acbef7bbb321d
ilu8ha <ilu8ha@gmail.com>
2024-06-14 00:53:02 +0300

    bugfix & clean code

--- src/main/java/com/ilu8ha/warps/FineWarps.java
+++ src/main/java/com/ilu8ha/warps/FineWarps.java
@@ -17,7 +17,7 @@ import java.util.*;
 public class FineWarps {
     public static final String MODID = "fmwarps";
     public static final String MODNAME = "FineWarps";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
     public static Logger logger;
     public static List<Warp> warpsData;
     public static HashMap<String, String > userNameCache;
@@ -87,5 +87,7 @@ public class FineWarps {
         }
         return false;
     }
+
+
 }
 
